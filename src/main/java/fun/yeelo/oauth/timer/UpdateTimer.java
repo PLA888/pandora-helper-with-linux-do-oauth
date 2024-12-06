ff469a3e8bdf9980fd0e74d1c5040f73ca5b5d9a
Kylsky <38944162+Kylsky@users.noreply.github.com>
2024-12-06 14:21:39 +0800

    优化定时任务

--- src/main/java/fun/yeelo/oauth/timer/UpdateTimer.java
+++ src/main/java/fun/yeelo/oauth/timer/UpdateTimer.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fun.yeelo.oauth.config.CommonConst;
-import fun.yeelo.oauth.config.HttpResult;
 import fun.yeelo.oauth.domain.*;
 import fun.yeelo.oauth.service.*;
 import fun.yeelo.oauth.utils.EncryptDecryptUtil;
@@ -23,7 +22,6 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.net.URLEncoder;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -96,89 +94,92 @@ public class UpdateTimer {
         });
     }
 
-    //@Scheduled(cron = "0 0 2 * * ?")
-    //public void updateRefreshToken() {
-    //    log.info("开始刷新access_token");
-    //    List<Account> accounts = accountService.list().stream()
-    //                                     .filter(e -> StringUtils.hasText(e.getRefreshToken()) && e.getAccountType().equals(1))
-    //                                     .collect(Collectors.toList());
-    //    accounts.forEach(account -> {
+    @Scheduled(cron = "0 0 2 * * ?")
+    public void updateRefreshToken() {
+        log.info("开始刷新access_token");
+        List<Account> accounts = accountService.list().stream()
+                                         .filter(e -> StringUtils.hasText(e.getRefreshToken()) && e.getAccountType().equals(1))
+                                         .collect(Collectors.toList());
+        accounts.forEach(account -> {
+            try {
+                Integer accountId = account.getId();
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                MultiValueMap<String, Object> personJsonObject = new LinkedMultiValueMap<>();
+                personJsonObject.add("refresh_token", account.getRefreshToken());
+                personJsonObject.add("redirect_uri", "com.openai.chat://auth0.openai.com/ios/com.openai.chat/callback");
+                personJsonObject.add("grant_type", "refresh_token");
+                personJsonObject.add("client_id", "pdlLIX2Y72MIl2rhLhTE9VV9bN905kBh");
+                ResponseEntity<String> stringResponseEntity = restTemplate.exchange("https://auth0.openai.com/oauth/token", HttpMethod.POST, new HttpEntity<>(personJsonObject, headers), String.class);
+                Map map = objectMapper.readValue(stringResponseEntity.getBody(), Map.class);
+                if (map.containsKey("access_token")) {
+                    log.info("refresh success");
+                    String newToken = map.get("access_token").toString();
+                    Account updateDTO = new Account();
+                    updateDTO.setId(accountId);
+                    updateDTO.setAccessToken(newToken);
+                    updateDTO.setUpdateTime(LocalDateTime.now());
+                    accountService.saveOrUpdate(updateDTO);
+                }
+            } catch (Exception e) {
+                log.error("刷新access_token异常,异常账号:{}", account.getEmail(), e);
+            }
+        });
+
+        if (mailEnable) {
+            accounts.forEach(account -> {
+                LocalDateTime updateTime = account.getUpdateTime();
+                if (updateTime != null && updateTime.toLocalDate().plusDays(9).isEqual(LocalDateTime.now().toLocalDate())) {
+                    emailService.sendSimpleEmail(adminEmail, "ACCESS_TOKEN过期提醒", "ACCESS_TOKEN即将过期,账号（邮箱）:"+account.getEmail());
+                }
+            });
+        }
+        log.info("刷新access_token结束");
+    }
+
+
+    //@Scheduled(cron = "0 0 3 */2 * ?")
+    //public void updateShareToken() {
+    //    log.info("开始刷新share_token");
+    //    List<Share> shares = shareService.list();
+    //
+    //    Map<Integer, Account> accountIdMap = accountService.list()
+    //                                                 .stream()
+    //                                                 .collect(Collectors.toMap(Account::getId, Function.identity()));
+    //
+    //    Map<Integer, ShareGptConfig> gptConfigMap = gptConfigService.list().stream().collect(Collectors.toMap(ShareGptConfig::getShareId, Function.identity()));
+    //
+    //    for (Share share : shares) {
+    //        ShareGptConfig gptConfig = gptConfigMap.get(share.getId());
+    //        if (gptConfig == null) {
+    //            continue;
+    //        }
     //        try {
-    //            Integer accountId = account.getId();
+    //            Share update = new Share();
+    //            update.setId(share.getId());
     //            HttpHeaders headers = new HttpHeaders();
     //            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
     //            MultiValueMap<String, Object> personJsonObject = new LinkedMultiValueMap<>();
-    //            personJsonObject.add("refresh_token", account.getRefreshToken());
-    //            ResponseEntity<String> stringResponseEntity = restTemplate.exchange(REFRESH_URL, HttpMethod.POST, new HttpEntity<>(personJsonObject, headers), String.class);
+    //            personJsonObject.add("access_token", accountIdMap.get(gptConfig.getAccountId()).getAccessToken());
+    //            personJsonObject.add("unique_name", share.getUniqueName());
+    //            personJsonObject.add("expires_in", 0);
+    //            personJsonObject.add("gpt35_limit", -1);
+    //            personJsonObject.add("gpt4_limit", -1);
+    //            personJsonObject.add("site_limit", "");
+    //            personJsonObject.add("show_userinfo", false);
+    //            personJsonObject.add("show_conversations", false);
+    //            personJsonObject.add("reset_limit", true);
+    //            personJsonObject.add("temporary_chat", false);
+    //            ResponseEntity<String> stringResponseEntity = restTemplate.exchange(CommonConst.SHARE_TOKEN_URL, HttpMethod.POST, new HttpEntity<>(personJsonObject, headers), String.class);
     //            Map map = objectMapper.readValue(stringResponseEntity.getBody(), Map.class);
-    //            if (map.containsKey("access_token")) {
-    //                log.info("refresh success");
-    //                String newToken = map.get("access_token").toString();
-    //                Account updateDTO = new Account();
-    //                updateDTO.setId(accountId);
-    //                updateDTO.setAccessToken(newToken);
-    //                updateDTO.setUpdateTime(LocalDateTime.now());
-    //                accountService.saveOrUpdate(updateDTO);
-    //            }
+    //            gptConfig.setShareToken(map.get("token_key").toString());
+    //            gptConfigService.updateById(gptConfig);
     //        } catch (Exception e) {
-    //            log.error("刷新access_token异常,异常账号:{}", account.getEmail(), e);
+    //            log.error("update share token error,unique_name:{}", share.getUniqueName(), e);
     //        }
-    //    });
-    //
-    //    if (mailEnable) {
-    //        accounts.forEach(account -> {
-    //            LocalDateTime updateTime = account.getUpdateTime();
-    //            if (updateTime != null && updateTime.toLocalDate().plusDays(9).isEqual(LocalDateTime.now().toLocalDate())) {
-    //                emailService.sendSimpleEmail(adminEmail, "ACCESS_TOKEN过期提醒", "ACCESS_TOKEN即将过期,账号（邮箱）:"+account.getEmail());
-    //            }
-    //        });
     //    }
-    //    log.info("刷新access_token结束");
+    //    log.info("刷新share_token结束");
     //}
-
-
-    @Scheduled(cron = "0 0 3 */2 * ?")
-    public void updateShareToken() {
-        log.info("开始刷新share_token");
-        List<Share> shares = shareService.list();
-
-        Map<Integer, Account> accountIdMap = accountService.list()
-                                                     .stream()
-                                                     .collect(Collectors.toMap(Account::getId, Function.identity()));
-
-        Map<Integer, ShareGptConfig> gptConfigMap = gptConfigService.list().stream().collect(Collectors.toMap(ShareGptConfig::getShareId, Function.identity()));
-
-        for (Share share : shares) {
-            ShareGptConfig gptConfig = gptConfigMap.get(share.getId());
-            if (gptConfig == null) {
-                continue;
-            }
-            try {
-                Share update = new Share();
-                update.setId(share.getId());
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-                MultiValueMap<String, Object> personJsonObject = new LinkedMultiValueMap<>();
-                personJsonObject.add("access_token", accountIdMap.get(gptConfig.getAccountId()).getAccessToken());
-                personJsonObject.add("unique_name", share.getUniqueName());
-                personJsonObject.add("expires_in", 0);
-                personJsonObject.add("gpt35_limit", -1);
-                personJsonObject.add("gpt4_limit", -1);
-                personJsonObject.add("site_limit", "");
-                personJsonObject.add("show_userinfo", false);
-                personJsonObject.add("show_conversations", false);
-                personJsonObject.add("reset_limit", true);
-                personJsonObject.add("temporary_chat", false);
-                ResponseEntity<String> stringResponseEntity = restTemplate.exchange(CommonConst.SHARE_TOKEN_URL, HttpMethod.POST, new HttpEntity<>(personJsonObject, headers), String.class);
-                Map map = objectMapper.readValue(stringResponseEntity.getBody(), Map.class);
-                gptConfig.setShareToken(map.get("token_key").toString());
-                gptConfigService.updateById(gptConfig);
-            } catch (Exception e) {
-                log.error("update share token error,unique_name:{}", share.getUniqueName(), e);
-            }
-        }
-        log.info("刷新share_token结束");
-    }
 
     @Scheduled(cron = "0 0 8 * * ?")
     public void sendShareExpiringEmail() {
@@ -215,7 +216,7 @@ public class UpdateTimer {
         for (Account account : accounts) {
             try {
                 LocalDateTime localDateTime = checkAccount(account.getAccessToken());
-                if (localDateTime.toLocalDate().isEqual(LocalDate.now().plusDays(3)) || localDateTime.toLocalDate().isBefore(LocalDate.now().plusDays(3))) {
+                if (localDateTime!=null && localDateTime.toLocalDate().isEqual(LocalDate.now().plusDays(3)) || localDateTime.toLocalDate().isBefore(LocalDate.now().plusDays(3))) {
                     emailService.sendSimpleEmail(account.getEmail(), "ChatGPT账号过期预警", "您的ChatGPT即将到期，到期时间为：" + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "，账号(邮箱):" + account.getEmail() + "，请注意及时续费。");
                 }
             } catch (Exception ex) {
@@ -255,30 +256,23 @@ public class UpdateTimer {
         headers.set("sec-fetch-user", "?1");
         headers.set("upgrade-insecure-requests", "1");
 
-        // 设置 Cookie
-        headers.set(HttpHeaders.COOKIE,
-                "oai-hlib=true; " +
-                        "oai-did=aa75acc8-54bb-4f83-bae7-c7a1b7eb66c7; " +
-                        "intercom-device-id-dgkjq2bp=bab4e9e8-ba51-4570-9992-259e438a19a7; " +
-                        "oai-nav-state=1; " +
-                        // ... 其他 cookie 值
-                        "_dd_s=rum=0&expire=1730870402848"
-        );
-
         // 创建请求实体
         HttpEntity<?> requestEntity = new HttpEntity<>(headers);
 
         try {
             // 发送请求
             ResponseEntity<String> response = restTemplate.exchange(
-                    "https://token.yeelo.fun/backend-api/accounts/check/v4-2023-04-27?timezone_offset_min=-480",
+                    "https://chatgpt.com/backend-api/accounts/check/v4-2023-04-27?timezone_offset_min=-480",
                     HttpMethod.GET,
                     requestEntity,
                     String.class
             );
+            if (!JSON.parseObject(response.getBody()).containsKey("accounts")) {
+                return null;
+            }
             return LocalDateTime.parse(JSON.parseObject(response.getBody()).getJSONObject("accounts").getJSONObject("default").getJSONObject("entitlement").getString("expires_at"), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"));
         } catch (RestClientException e) {
-            throw new RuntimeException("账户请求失败: " + e.getMessage());
+            return null;
         }
     }
 
