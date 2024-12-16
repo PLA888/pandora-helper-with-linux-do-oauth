dfeaf7ad7cc79793f8c33f7688a736cfdf6dd087
Kylsky <38944162+Kylsky@users.noreply.github.com>
2024-12-16 14:28:31 +0800

    优化分页栏

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-docker buildx build --platform linux/amd64 -t pandora_helper_v2:latest --load .
-docker tag pandora_helper_v2:latest kylsky/pandora_helper_v2:latest
-#docker save -o pandora_helper_v2.tar pandora_helper_v2:latest
+docker buildx build --platform linux/amd64 -t kylsky/pandora_helper_v2:latest --load .
+docker push kylsky/pandora_helper_v2:latest
